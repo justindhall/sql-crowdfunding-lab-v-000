5119b4838f30f63c3f108911b5e8200ddc85395c
Justin Hall <hall.justin.d@gmail.com>
2019-04-17 01:18:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,39 @@
 INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Sue", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
+
+INSERT INTO users (name, age) VALUES ("Joe", 30);
