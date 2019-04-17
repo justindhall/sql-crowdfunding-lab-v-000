688a7a712ec47c370c3fb1ead9505b4e5158e732
Justin Hall <hall.justin.d@gmail.com>
2019-04-17 01:18:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO users (name, age) VALUES ("Joe", 30);
 
 INSERT INTO users (name, age) VALUES ("Sue", 30);
 
-INSERT INTO users (name, age) VALUES ("Joe", 30);
+INSERT INTO users (name, age) VALUES ("Alex", 30);
 
 INSERT INTO users (name, age) VALUES ("Joe", 30);
 
